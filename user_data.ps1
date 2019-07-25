a146f2a8342b951988daeb116d6c11754ec01800
dgulli <dgulli@gmail.com>
2019-07-25 14:03:22 +1000

    using winrm https

--- user_data.ps1
+++ user_data.ps1
@@ -11,12 +11,17 @@ $ErrorActionPreference = "stop"
 # Remove HTTP listener
 Remove-Item -Path WSMan:\Localhost\listener\listener* -Recurse
 
+Set-Item WSMan:\localhost\MaxTimeoutms 1800000
+Set-Item WSMan:\localhost\Service\Auth\Basic $true
+
+$Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName "packer"
+New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $Cert.Thumbprint -Force
+
 # WinRM
 write-output "Setting up WinRM"
 write-host "(host) setting up WinRM"
 
 cmd.exe /c winrm quickconfig -q
-cmd.exe /c winrm quickconfig '-transport:http'
 cmd.exe /c winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
 cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="1024"}'
 cmd.exe /c winrm set "winrm/config/service" '@{AllowUnencrypted="true"}'
@@ -24,12 +29,11 @@ cmd.exe /c winrm set "winrm/config/client" '@{AllowUnencrypted="true"}'
 cmd.exe /c winrm set "winrm/config/service/auth" '@{Basic="true"}'
 cmd.exe /c winrm set "winrm/config/client/auth" '@{Basic="true"}'
 cmd.exe /c winrm set "winrm/config/service/auth" '@{CredSSP="true"}'
-cmd.exe /c winrm set "winrm/config/listener?Address=*+Transport=HTTP" '@{Port="5985"}'
+cmd.exe /c winrm set "winrm/config/listener?Address=*+Transport=HTTPS" "@{Port=`"5986`";Hostname=`"packer`";CertificateThumbprint=`"$($Cert.Thumbprint)`"}"
 cmd.exe /c netsh advfirewall firewall set rule group="remote administration" new enable=yes
-cmd.exe /c netsh firewall add portopening TCP 5985 "Port 5985"
+cmd.exe /c netsh firewall add portopening TCP 5986 "Port 5986"
 cmd.exe /c net stop winrm
 cmd.exe /c sc config winrm start= auto
 cmd.exe /c net start winrm
-cmd.exe /c wmic useraccount where "name='vagrant'" set PasswordExpires=FALSE
 
 </powershell>
